4fe24cd93c269717eb898c44b7593a2d6049db20
idoko <radpangolin@gmail.com>
2020-06-26 20:28:45 +0100

    sync route path with post

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PostsController@index')->name('index');
 Route::get('/posts/{id}', 'PostsController@show')->name('view-post');
 Route::post('/posts/receive-email-response', 'PostsController@receiveEmailResponse');
-Route::get('send-mail', 'PostsController@sendMails');
+Route::get('send-email', 'PostsController@sendMails');
